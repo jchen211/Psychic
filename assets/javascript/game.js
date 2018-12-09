1b49f9d0bb7ed0a277080b9811cec1453dd26e33
Jessica Chen <jchen@jchen.attlocal.net>
2018-12-09 00:18:13 -0600

    script in wrong area. added last in body

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,21 @@
-var userChoiceText = document.getElementById("userchoice-text");
-var compChoiceText = document.getElementById("compchoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesLeft-text");
-var guessesText = document.getElementById("guesses-text");
-
-
- var compOptions = 
-["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+var compOptions = 
+ ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
  "k","l","m","n","o","p","q","r","s","t","u","v",
-"w","x","y","z"]; 
+ "w","x","y","z"]; 
 
 var wins = 0;
 var losses = 0;
 var guessLeft = 9;
 var guessedLetter = [];
 
-document.onkeyup = function(event) {
+var userChoiceText = document.getElementById("userchoice-text");
+var compChoiceText = document.getElementById("compchoice-text");
+var winsText = document.getElementById("wins-text")  
+var lossesText = document.getElementById("losses-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var guessesText = document.getElementById("guesses-text");
+
+document.onkeyup= function(event) {
     var userChoice = event.key;
     guessedLetter.push(userChoice);
 
@@ -36,11 +35,10 @@ document.onkeyup = function(event) {
      guessedLetter.length = 0;
    }
 
-    userChoiceText = "User Choice: " + userChoice;
-    compChoiceText = "Computer Choice: " + compChoice ;
-    winsText = "Wins: " + wins;
-    lossesText = "Losses: " + losses;
-    guessesLeftText = "Guesses Left: " + guessLeft;
-    guessesText = "Letters Guessed: " + (guessedLetter);
-    
-}
+    userChoiceText.textContent = "Computer Choice: " + userChoice;
+    compChoiceText.textContent = "Computer Choice: " + compChoice ;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessesLeftText.textContent = "Guesses Left: " + guessLeft;
+    guessesText.textContent = "Letters Guessed: " + (guessedLetter);
+   }
